012a0f3b00fb7f6970358d3224385ef9c78d4603
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-02 08:32:05 -0600

    Seeding completed for Station, foreign keys for station_name, city_name link, installation_date needs to be updated

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,8 +2,8 @@ class Station < ActiveRecord::Base
   belongs_to :city
   belongs_to :bike_share_date
   belongs_to :station_name
-  validates :name, presence: true
+  validates :station_name_id, presence: true
   validates :dock_count, presence: true
-  validates :city, presence: true
-  validates :installation_date, presence: true
+  validates :city_id, presence: true
+  validates :installation_date_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,25 @@ require 'active_support/core_ext/time'
 require './app/models/city'
 require './app/models/station_name'
 require './app/models/bike_share_date'
+require './app/models/station'
 
 City.destroy_all
 StationName.destroy_all
 BikeShareDate.destroy_all
+Station.destroy_all
 
 station_text = File.read('./db/csv/station.csv')
 station_csv = CSV.parse(station_text, :headers => true, header_converters: :symbol)
 station_csv.each do |row|
-  station_names = StationName.find_or_create_by!(name: row[:name])
-  date = DateTime.strptime(row[:installation_date], '%m/%d/%Y')
-  station_dates = BikeShareDate.find_or_create_by!(date: date)
-  city = City.find_or_create_by!(name: row[:city])
+  station_name      = StationName.find_or_create_by!(name: row[:name])
+  date              = DateTime.strptime(row[:installation_date], '%m/%d/%Y')
+  installation_date = BikeShareDate.find_or_create_by!(date: date)
+  city              = City.find_or_create_by!(name: row[:city])
+  Station.find_or_create_by!(station_id:           row[:id],
+                             station_name_id:      station_name.id,
+                             lat:                  row[:lat],
+                             long:                 row[:long],
+                             dock_count:           row[:dock_count],
+                             city_id:              city.id,
+                             installation_date_id: installation_date.id)
 end
