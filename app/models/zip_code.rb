706d8f8a44ab44656dc117b3e25e41870bbd00ba
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-02 15:08:06 -0600

    Updated zip_code column to 8 digits to account for user entry error

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,3 +1,4 @@
 class ZipCode < ActiveRecord::Base
   has_many :trips
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702143436) do
+ActiveRecord::Schema.define(version: 20170702210245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 20170702143436) do
   end
 
   create_table "zip_codes", force: :cascade do |t|
-    t.integer  "zip_code"
+    t.bigint   "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
