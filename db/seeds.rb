8fd983ecf592dc9e871ea95eaa6975c613ec1b96
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-02 07:25:19 -0600

    Refactored CSV import for City, StationName, BikeShareDate

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,11 @@ City.destroy_all
 StationName.destroy_all
 BikeShareDate.destroy_all
 
-city_text = File.read('./db/csv/city.csv')
-city_csv = CSV.parse(city_text, :headers => true, header_converters: :symbol)
-city_csv.each do |row|
-  city = City.find_or_create_by!(name: row[:name])
-end
-
 station_text = File.read('./db/csv/station.csv')
-station_names_csv = CSV.parse(station_text, :headers => true, header_converters: :symbol)
-station_names_csv.each do |row|
+station_csv = CSV.parse(station_text, :headers => true, header_converters: :symbol)
+station_csv.each do |row|
   station_names = StationName.find_or_create_by!(name: row[:name])
-end
-
-station_dates_csv =  CSV.parse(station_text, :headers => true, header_converters: :symbol)
-station_dates_csv.each do |row|
   date = DateTime.strptime(row[:installation_date], '%m/%d/%Y')
   station_dates = BikeShareDate.find_or_create_by!(date: date)
+  city = City.find_or_create_by!(name: row[:city])
 end
